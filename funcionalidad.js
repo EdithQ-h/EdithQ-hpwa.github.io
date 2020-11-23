2de7bda44c521b1dbf19d81614d4fdf2b1341c1b
EdithQ-h <61257173+EdithQ-h@users.noreply.github.com>
2020-11-23 06:12:32 -0600

    Update funcionalidad.js

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,40 +1,40 @@
-función  operaciones ( op )
+function operaciones(op)
 {
 
-    var  ops  =  {
+    var ops = {
       
 
-       densidad : function  dividirNumeros ( n1 ,  n2 )  {
-            return  ( parseInt ( n1 ) / parseInt ( n2 ) ) ;
+       densidad: function dividirNumeros(n1, n2) {
+            return (parseInt(n1) / parseInt(n2));
         }
 
 
-    } ;
+    };
 
 
 
-    var  num1  =  documento . getElementById ( "num1" ) . valor ;
-    var  num2  =  documento . getElementById ( "num2" ) . valor ;
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
 
     
-    // Comprobamos si se ha introducido números en las cajas
-    if  ( isNaN ( parseFloat ( document . getElementById ( 'num1' ) . value ) ) )  {
-        documento . getElementById ( 'resultado' ) . innerHTML = "<span style = 'color: red;'> Por favor, escriba el primer numero </span>" ;
-        documento . getElementById ( "num1" ) . innerText  =  "0" ;
-        documento . getElementById ( "num1" ) . foco ( ) ;
-        }  else  if  ( isNaN ( parseFloat ( document . getElementById ( 'num2' ) . value ) ) )  {
-        documento . getElementById ( 'resultado' ) . innerHTML = "<span style = 'color: red;'> Por favor, escriba el segundo numero </span>" ;
-        documento . getElementById ( "num2" ) . innerText  =  "0" ;
-        documento . getElementById ( "num2" ) . foco ( ) ;
+    //Comprobamos si se ha introducido números en las cajas
+    if (isNaN(parseFloat(document.getElementById('num1').value))) {
+        document.getElementById('resultado').innerHTML="<span style='color: red;'>Por favor, ingrese el primer numero</span>";
+        document.getElementById("num1").innerText = "0";
+        document.getElementById("num1").focus();
+        } else if (isNaN(parseFloat(document.getElementById('num2').value))) {
+        document.getElementById('resultado').innerHTML="<span style='color: red;'>Por favor, ingrese el segundo numero</span>";
+        document.getElementById("num2").innerText = "0";
+        document.getElementById("num2").focus();
     }
-    else  {
-    // Si se han introducido los números en ámbas cajas, operamos:
-        cambiar ( op )  {
+    else {
+    //Si se han introducido los números en ámbas cajas, operamos:
+        switch(op) {
             
-            caso  'densidad' :
-                var  resultado  =  ops . densidad ( num1 ,  num2 ) ;
-                documento . getElementById ( 'resultado' ) . innerHTML = "<span style = 'color: green;'>" + resultado + "</span>" ;
-                romper ;
+            case 'densidad':
+                var resultado = ops.densidad(num1, num2);
+                document.getElementById('resultado').innerHTML="<span style='color: green;'>"+resultado+"</span>";
+                break;
 
         }
     }
